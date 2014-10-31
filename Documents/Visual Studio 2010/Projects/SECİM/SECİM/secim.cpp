05e720f62712846087c3df5a7bad0cdc5ef65058
cylnclk <ceylan3313@hotmail.com>
2014-10-31 19:24:08 +0200

    for döngüsüne örnekler

---
+++ Documents/Visual Studio 2010/Projects/SECİM/SECİM/secim.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+	/*char tur;
+	int gun,km,ucret;
+	printf("araba turleri\n(A veya a)Arazi arabalarý\n(B veya b)binek arabalarý\n(S veya s)satation arabalarý\n(P veya p)spor arabalar");
+	printf("\n\n\nArabaturu:");
+	scanf("%c",&tur);
+	printf("arabayý kac gun kullanacaksýnýz\n");
+	scanf("%d",&gun);
+	printf("arabayý kac km kullanacaksýnýz\n");
+	scanf("%d",&km);
+	switch(tur){
+	case'a':case'A':
+	ucret=(gun*43)+(km*18);
+	break;
+	case'b':case'B':
+		ucret=(gun*32)+(km*22);
+		break;
+	case's':case'S':
+		ucret=(gun*43)+(km*28);
+		break;
+	case'p':case'P':
+		ucret=(gun*51)+(km*36);
+		break;
+	
+	}
+	printf("odemeniz gereken ucret:%d$",ucret);
+
+	*/
+	
+	
+
+	
+
+
+
+
+
+
+
+
+
+}
